a79467fb8ef3f63d47939f9c0ed4b52bb01f5885
Stacy Lee <stacy.lee92@gmail.com>
2025-05-19 18:49:21 -0700

    finished referencing solution code

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,57 @@
-import { episodeList } from "./data.js";
-import Episodes from "./components/Episodes/Episodes";
-import SelectedEpisode from "./components/SelectedEpisode/SelectedEpisode";
-console.log(episodeList);
+import { useState } from "react";
+import { episodeList } from "./data";
 
 export default function App() {
+  const [episodes] = useState(episodeList);
+  const [selectedEpisode, setSelectedEpisode] = useState();
+
+  function EpisodeDetails() {
+    if (!selectedEpisode) {
+      return (
+        <section className="details">
+          <h2>Episode Details</h2>
+          <p>Select an episode to learn more.</p>
+        </section>
+      );
+    }
+    return (
+      <section className="details">
+        <h3>Episode {selectedEpisode.id}</h3>
+        <h3>{selectedEpisode.title}</h3>
+        <p>{selectedEpisode.description}</p>
+        <button>Watch Now</button>
+      </section>
+    );
+  }
+
+  function EpisodeList() {
+    return (
+      <section className="episodes">
+        <h2>Episodes</h2>
+        <ol>
+          {episodes.map((episode) => (
+            <li
+              key={episode.id}
+              onClick={() => setSelectedEpisode(episode)}
+              className={selectedEpisode?.id === episode.id ? "selected" : ""}
+            >
+              {episode.title}
+            </li>
+          ))}
+        </ol>
+      </section>
+    );
+  }
+
   return (
     <>
-      <div>
+      <header>
         <h1>Dark Echoes</h1>
-        <Episodes episodes={episodeList} />
-        <SelectedEpisode />
-      </div>
+      </header>
+      <main>
+        <EpisodeList />
+        <EpisodeDetails />
+      </main>
     </>
   );
 }
